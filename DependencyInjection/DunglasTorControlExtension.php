cf7618b3fa20a17e764a6e28e70d54f581078a86
Kévin Dunglas <dunglas@gmail.com>
2013-08-25 18:26:16 +0200

    Add licence headers

--- DependencyInjection/DunglasTorControlExtension.php
+++ DependencyInjection/DunglasTorControlExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the TorControl package.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Dunglas\TorControlBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
